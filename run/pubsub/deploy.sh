54580bbb3f13289d735edefc4a489eca4a54cab6
Eric Haase <eric.haase@wpengine.com>
2023-04-04 15:23:52 -0500

    added payload to test locally

--- run/pubsub/deploy.sh
+++ run/pubsub/deploy.sh
@@ -4,5 +4,3 @@ set -o pipefail  # don't hide errors within pipes
 
 gcloud builds submit --tag gcr.io/wpe-cr-dev/bigquery-slack-notifier
 gcloud run deploy bigquery-slack-notifier --image gcr.io/wpe-cr-dev/bigquery-slack-notifier:latest --no-allow-unauthenticated --region us-east1 --update-secrets SLACK_WEBHOOK_URL=slack-webhook-url-bigquery-notifier:latest
-gcloud pubsub topics publish projects/wpe-platform-one-dev/topics/cron-monitoring --message '{ "state": "SUCCEEDED", "params": { "destination_table_name_template": "daily_usage_metrics_summary" } }'
-gcloud pubsub topics publish projects/wpe-platform-one-dev/topics/cron-monitoring --message '{ "state": "FAILED", "params": { "destination_table_name_template": "p1adb_current_vs_last_week_comparison" }, "errorStatus": { "code": 3, "message": "On 2023-03-29 the difference between this week and last week metrics was not calculated ; JobID: 892230234532:scheduled_query_6454f936-0000-2289-811e-f403045e5844" } }'
